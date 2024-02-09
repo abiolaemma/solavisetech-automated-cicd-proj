c55c2d7aea84a3f850baa58c586ecaf76a6ac654
Emmanuel <abiolaemma@gmail.com>
2024-02-09 14:33:21 -0500

    modified

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -77,10 +77,10 @@ resource "aws_instance" "web_server" {
   tags                   = merge(var.tags, { Name = join("", [var.name, "-", "webserver"]) }, { Environment = var.name })
 
   # best practices as per checkov scanner
-  monitoring    = true
-  ebs_optimized = true
-   root_block_device {
-     encrypted = true
-   }
+  # monitoring    = true
+  # ebs_optimized = true
+  #  root_block_device {
+  #    encrypted = true
+  #  }
 
 }
